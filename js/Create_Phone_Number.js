03374c5bd9e1e11911283e053794e13231972958
mpsujek <mateusz.psujek@gmail.com>
2019-11-26 11:41:12 +0100

    Added prittier config file

--- js/Create_Phone_Number.js
+++ js/Create_Phone_Number.js
@@ -1,13 +1,13 @@
 // https://www.codewars.com/kata/create-phone-number/train/javascript
 
-function createPhoneNumber(numbers){
-  if (numbers.length === 10){
-    const a = numbers.slice(0,3).join('');
-    const b = numbers.slice(3,6).join('');
-    const c = numbers.slice(6,10).join('');
+function createPhoneNumber(numbers) {
+  if (numbers.length === 10) {
+    const a = numbers.slice(0, 3).join('');
+    const b = numbers.slice(3, 6).join('');
+    const c = numbers.slice(6, 10).join('');
     return (`(${a})${b}-${c}`);
-    
+
   } else {
     return new Error('Array length is not equal 10')
   }
-}  
+}
