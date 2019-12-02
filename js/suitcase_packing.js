2245c2b12bf15721d7b68d0605690e21fb73dd67
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-02 21:12:14 +0100

    Simplified solution

--- js/suitcase_packing.js
+++ js/suitcase_packing.js
@@ -1,10 +1,5 @@
 //https://www.codewars.com/kata/suitcase-packing/train/javascript
 
 function fit_in(a, b, m, n) {
-  if((Math.pow(a,2) + Math.pow(b,2)) <= m * n){
-      return true;
-  }
-  else{
-      return false;
-  }
+ return ((Math.pow(a,2) + Math.pow(b,2)) <= m * n);
 }
