b0477765ad0d2b8ea6697793e545776709da7dea
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-02 21:17:12 +0100

    Simplified function

--- js/youre_square.js
+++ js/youre_square.js
@@ -1,10 +1,5 @@
 // https://www.codewars.com/kata/youre-a-square/train/javascript
 
 var isSquare = function(n){
-  if(Math.sqrt(n) !== NaN && Number.isInteger(Math.sqrt(n))){
-      return true
-  }
-  else{
-      return false;
-  }
+  return (Number.isInteger(Math.sqrt(n)))
 }
