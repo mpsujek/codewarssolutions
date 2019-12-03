2cd71684584b62a284e1f1039c64618385896d8a
mpsujek <mateusz.psujek@gmail.com>
2019-12-03 22:01:42 +0100

    Solution for codewars testing 1-2-3 done

---
+++ js/testing_1_2_3.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/testing-1-2-3/train/javascript
+
+var number=function(array){
+  let i = 1;
+  const newArray = array.map(function(el){
+     el = `${i}: ${el}`;
+     i++;
+     return el;
+    })
+
+  return newArray;
+}
