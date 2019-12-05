8e11a5ca7457f7f5774bec3eb661f74356390a46
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-05 22:48:36 +0100

    Solution for remove string spaces with tests

---
+++ js/remove_string_spaces.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/remove-string-spaces/train/javascript
+
+function noSpace(string){
+  const array = string.split(' ');
+  const newArray = array.filter( function(el){
+      return el.length > 0;
+  })
+return newArray.join('');
+
+}
+
+module.exports = noSpace;
