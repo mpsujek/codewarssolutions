4e39fb33bc18ad8586591780d91c669535d1ccd4
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-09 22:32:11 +0100

    Solution refactor removed unneeded code

--- js/testing_1_2_3.js
+++ js/testing_1_2_3.js
@@ -1,14 +1,10 @@
 // https://www.codewars.com/kata/testing-1-2-3/train/javascript
 
-var number=function(array){
-  let i = 1;
-  const newArray = array.map(function(el){
-     el = `${i}: ${el}`;
-     i++;
+const number=function(array){
+  return array.map(function(el, i){
+     el = `${i+1}: ${el}`;
      return el;
     })
-
-  return newArray;
 }
 
 module.exports = number;
