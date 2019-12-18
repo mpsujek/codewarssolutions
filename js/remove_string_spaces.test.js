8772531c58d783a0b41f38cf69140bdc8f2a23ac
mpsujek <mateusz.psujek@gmail.com>
2019-12-18 23:26:11 +0100

    Changes of tests naming

--- js/remove_string_spaces.test.js
+++ js/remove_string_spaces.test.js
@@ -3,11 +3,11 @@ const noSpaces = require('./remove_string_spaces');
 
 describe('remove_string_spaces', () => {
 
-  test('1 case', () => {
+  test('Remove of single spaces', () => {
     expect(noSpaces('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')).toBe('88Bifk8hB8BB8BBBB888chl8BhBfd');
   });
 
-  test('2 case', () => {
+  test('Remove of multiple spaces', () => {
     expect(noSpaces('8 j 8   mBliB8g  imjB8B8  jl  B')).toBe('8j8mBliB8gimjB8B8jlB');
   });
 });
