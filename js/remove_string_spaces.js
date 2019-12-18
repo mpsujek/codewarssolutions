bbeea321ac7de2363586cf70c8fcd9e38f274583
mpsujek <mateusz.psujek@gmail.com>
2019-12-18 23:27:47 +0100

    Formatting changes - plugin neds reload

--- js/remove_string_spaces.js
+++ js/remove_string_spaces.js
@@ -1,11 +1,11 @@
 // https://www.codewars.com/kata/remove-string-spaces/train/javascript
 
-function noSpace(string){
+function noSpace(string) {
   const array = string.split(' ');
-  const newArray = array.filter( function(el){
-      return el.length > 0;
+  const newArray = array.filter(function (el) {
+    return el.length > 0;
   })
-return newArray.join('');
+  return newArray.join('');
 
 }
 
