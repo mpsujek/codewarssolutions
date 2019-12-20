1220b95d49862fdf6276800808cea60b222a2980
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-20 23:28:01 +0100

    Added new solution for count monkeys

--- js/count_monkeys.js
+++ js/count_monkeys.js
@@ -2,11 +2,17 @@
 
 function monkeyCount(n) {
   const array = [];
-  let i = 1;
-  do{
-     array.push(i);
-     i++;
-  } while (i < n + 1);
+  // first solution
+  // let i = 1;
+  // do{
+  //    array.push(i);
+  //    i++;
+  // } while (i < n + 1);
+
+  //
+  for (let i = 1; i < n + 1 ; i++) {
+    array.push(i);
+  }
   return array;
  }
 
