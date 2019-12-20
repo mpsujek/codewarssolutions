b9ae4531abe573be360eca147570512b53ced70c
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-20 23:24:32 +0100

    Change of test namings

--- js/count_monkeys.test.js
+++ js/count_monkeys.test.js
@@ -3,11 +3,11 @@ const monkeyCount = require('./count_monkeys');
 
 describe('count_monkeys', () => {
 
-  test(' 1 monkeyCount', () => {
+  test(' When it get 5 it should return array of numbers from 1 to 5', () => {
     expect(monkeyCount(5)).toStrictEqual([1, 2, 3, 4, 5]);
   });
 
-  test(' 2 monkeyCount', () => {
+  test('When it get 20 it should return array of numbers from 1 do 20', () => {
     expect(monkeyCount(20)).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
   });
 });
