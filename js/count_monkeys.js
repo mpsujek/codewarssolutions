8df245ee205839c74d9ca80224437f1da41009ae
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-20 23:36:45 +0100

    Solution with Array.from for count monkeys

--- js/count_monkeys.js
+++ js/count_monkeys.js
@@ -2,18 +2,22 @@
 
 function monkeyCount(n) {
   const array = [];
-  // first solution
+  // solution with do while
   // let i = 1;
   // do{
   //    array.push(i);
   //    i++;
   // } while (i < n + 1);
 
-  //
-  for (let i = 1; i < n + 1 ; i++) {
-    array.push(i);
-  }
-  return array;
+  // solution with for loop
+  // for (let i = 1; i < n + 1 ; i++) {
+  //   array.push(i);
+  // }
+  // return array;
+
+  // solution with Array.from
+  return  Array.from({length : n}, (e, i ) => i + 1);
+
  }
 
  module.exports = monkeyCount;
