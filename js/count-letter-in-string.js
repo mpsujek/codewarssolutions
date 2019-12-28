f0b9c363be5d8fab69f84dce04d915815aeaaa20
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-28 21:26:53 +0100

    added test for this cata

--- js/count-letter-in-string.js
+++ js/count-letter-in-string.js
@@ -1,17 +1,16 @@
 // https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
 
-function count (string) {
+function count(string) {
   const lettersArr = string.split('');
- let lettersCount = {};
- for(let i = 0; i < lettersArr.length; i++){
-      if(lettersCount[lettersArr[i]] == undefined){
-              lettersCount[lettersArr[i]] = 1;
-      }
-      else{
-         lettersCount[lettersArr[i]]++;
-      }
-
- }
- return lettersCount;
+  let lettersCount = {};
+  for (let i = 0; i < lettersArr.length; i++) {
+    if (lettersCount[lettersArr[i]] == undefined) {
+      lettersCount[lettersArr[i]] = 1;
+    } else {
+      lettersCount[lettersArr[i]]++;
+    }
+  }
+  return lettersCount;
 }
 
+module.exports = count;
