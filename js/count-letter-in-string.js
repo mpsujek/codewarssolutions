106486a9b436903308971aab7c49e87224032f64
Mateusz Psujek <mateusz.psujek@gmail.com>
2019-12-28 21:08:36 +0100

    New solution for cata count letter in string

---
+++ js/count-letter-in-string.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
+
+function count (string) {
+  const lettersArr = string.split('');
+ let lettersCount = {};
+ for(let i = 0; i < lettersArr.length; i++){
+      if(lettersCount[lettersArr[i]] == undefined){
+              lettersCount[lettersArr[i]] = 1;
+      }
+      else{
+         lettersCount[lettersArr[i]]++;
+      }
+
+ }
+ return lettersCount;
+}
+
