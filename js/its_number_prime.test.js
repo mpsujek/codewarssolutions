593b510be088e1ab78985a408f2640f1c7b02ce4
mpsujek <mateusz.psujek@gmail.com>
2020-01-08 22:49:26 +0100

    Changes in tests naming

--- js/its_number_prime.test.js
+++ js/its_number_prime.test.js
@@ -3,23 +3,23 @@ const isPrime = require('./its_number_prime');
 
 describe('its_number_prime', () => {
 
-  test('test 0 false', () => {
+  test('If get 0 should return false', () => {
     expect(isPrime(0)).toBe(false);
   });
 
-  test('test 1 false', () => {
+  test('If get 1 should return false', () => {
     expect(isPrime(1)).toBe(false);
   });
 
-  test('test 2 true', () => {
+  test('If get 2 should return true', () => {
     expect(isPrime(2)).toBe(true);
   });
 
-  test('test 73 true', () => {
+  test('If get 73 should return true', () => {
     expect(isPrime(73)).toBe(true);
   });
 
-  test('test 5099 true ', () => {
+  test('If get 5099 should return true ', () => {
     expect(isPrime(5099)).toBe(true);
   });
 
