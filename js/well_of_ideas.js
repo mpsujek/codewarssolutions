957ff78ac2434491b9c37ed6c9f78bb09b553e8c
mpsujek <mateusz.psujek@gmail.com>
2020-01-10 15:13:49 +0100

    Naming improvlement and link to cata added

--- js/well_of_ideas.js
+++ js/well_of_ideas.js
@@ -1,20 +1,21 @@
-function well(x){
+// http://codewars.com/kata/57f222ce69e09c3630000212/train/javascript
+
+function well(x) {
   const text = x.filter(isEqualGood);
 
-  if(text.length > 2 ){
+  if (text.length > 2) {
     return 'I smell a series!';
   }
-  if(text.length >= 1){
+  if (text.length >= 1) {
     return 'Publish!';
-  }
-  else {
+  } else {
     return 'Fail!';
   }
 }
 
 
 function isEqualGood(value) {
-return value === 'good';
+  return value === 'good';
 }
 
 module.exports = well;
--- js/well_of_ideas.test.js
+++ js/well_of_ideas.test.js
@@ -3,15 +3,15 @@ const well = require('./well_of_ideas');
 
 describe('well_of_ideas', () => {
 
-  test('should be Fail', () => {
+  test('When it gest only bad ideas should be Fail', () => {
     expect(well(['bad', 'bad', 'bad'])).toBe('Fail!');
   });
 
-  test('should be Publish', () => {
+  test('If its get minimum one or two good ideas should be Publish', () => {
     expect(well(['good', 'bad', 'bad', 'bad', 'bad'])).toBe('Publish!');
   });
 
-  test('should be ', () => {
+  test('If its more than two good ideas should return I smell a series ', () => {
     expect(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good'])).toBe('I smell a series!');
   });
 
