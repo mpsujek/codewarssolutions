ef2c0501e250b166b14b04d8e7b719c19e36144a
mpsujek <mateusz.psujek@gmail.com>
2020-01-10 15:07:32 +0100

    Change in tests naming

--- js/lost_without_map.test.js
+++ js/lost_without_map.test.js
@@ -1,13 +1,13 @@
 const maps = require('./lost_without_map');
 
 
-describe('maps', () => {
+describe('lost without map tests', () => {
 
-  test('test NaN', () => {
+  test('Test-1 for simple multiplying 3 elements array', () => {
     expect(maps([1, 2, 3])).toStrictEqual([2, 4, 6]);
   });
 
-  test('test false', () => {
+  test('Test-2 for simple multiplying 5 element array', () => {
     expect(maps([4, 1, 1, 1, 4])).toStrictEqual([8, 2, 2, 2, 8]);
   });
 
