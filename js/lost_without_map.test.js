9537ab5cf9cb45f30adaf00258a52b5af7cacf74
Mateusz Psujek <mateusz.psujek@gmail.com>
2020-01-12 12:57:57 +0100

    formatting correction

--- js/lost_without_map.test.js
+++ js/lost_without_map.test.js
@@ -1,8 +1,6 @@
 const maps = require('./lost_without_map');
 
-
 describe('lost without map tests', () => {
-
   test('Should return 3 elements array with multiplied elements by 2', () => {
     expect(maps([1, 2, 3])).toStrictEqual([2, 4, 6]);
   });
@@ -10,5 +8,4 @@ describe('lost without map tests', () => {
   test('Should return 5 elements array with multiplied elements by 2', () => {
     expect(maps([4, 1, 1, 1, 4])).toStrictEqual([8, 2, 2, 2, 8]);
   });
-
 });
