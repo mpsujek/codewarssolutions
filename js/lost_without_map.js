9a54da9f3e29f0457ee57a1d16d9527466f87393
Mateusz Psujek <mateusz.psujek@gmail.com>
2020-01-12 12:57:44 +0100

    Removed uneeded files, formatting correction, naming of test cases change

--- js/lost_without_map.js
+++ js/lost_without_map.js
@@ -1,8 +1,9 @@
 // https://www.codewars.com/kata/beginner-lost-without-a-map/train/javascript
 
-function maps(x){
-  return x.map(function(el){
-   return el*2})
- }
+function maps(x) {
+  return x.map(function(el) {
+    return el * 2;
+  });
+}
 
 module.exports = maps;
--- js/lost_without_map.test.js
+++ js/lost_without_map.test.js
@@ -3,11 +3,11 @@ const maps = require('./lost_without_map');
 
 describe('lost without map tests', () => {
 
-  test('Test-1 for simple multiplying 3 elements array', () => {
+  test('Should return 3 elements array with multiplied elements by 2', () => {
     expect(maps([1, 2, 3])).toStrictEqual([2, 4, 6]);
   });
 
-  test('Test-2 for simple multiplying 5 element array', () => {
+  test('Should return 5 elements array with multiplied elements by 2', () => {
     expect(maps([4, 1, 1, 1, 4])).toStrictEqual([8, 2, 2, 2, 8]);
   });
 
