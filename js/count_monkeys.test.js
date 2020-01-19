e864c4f2ea713d93a1a76914c37a8c76df769829
mpsujek <mateusz.psujek@gmail.com>
2020-01-19 23:51:41 +0100

    Changes in test function using each

--- js/count_monkeys.test.js
+++ js/count_monkeys.test.js
@@ -5,92 +5,39 @@ const {
 } = require('./count_monkeys');
 
 describe('count_monkeys do while function', () => {
-  test('returns an array of numbers from 1 to 5 when given 5', () => {
-    expect(monkeyCountDoWhile(5)).toStrictEqual([1, 2, 3, 4, 5]);
-  });
-
-  test('returns an array of n~umbers from 1 to 20 when given 20', () => {
-    expect(monkeyCountDoWhile(20)).toStrictEqual([
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-    ]);
-  });
+  test.each`
+    input | expectedResult
+    ${5}  | ${[1, 2, 3, 4, 5]}
+    ${20} | ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
+  `(
+    'converts $input to $expectedResult with config: $configDescription',
+    ({ input, expectedResult }) => {
+      expect(monkeyCountDoWhile(input)).toStrictEqual(expectedResult);
+    },
+  );
 });
-describe('count_monkeys for loop', () => {
-  test('returns an array of numbers from 1 to 5 when given 5', () => {
-    expect(monkeyCountForLoop(5)).toStrictEqual([1, 2, 3, 4, 5]);
-  });
 
-  test('returns an array of numbers from 1 to 20 when given 20', () => {
-    expect(monkeyCountForLoop(20)).toStrictEqual([
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-    ]);
-  });
+describe('count_monkeys for loop', () => {
+  test.each`
+    input | expectedResult
+    ${5}  | ${[1, 2, 3, 4, 5]}
+    ${20} | ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
+  `(
+    'converts $input to $expectedResult with config: $configDescription',
+    ({ input, expectedResult }) => {
+      expect(monkeyCountForLoop(input)).toStrictEqual(expectedResult);
+    },
+  );
 });
 describe('count_monkeys from', () => {
-  test('returns an array of numbers from 1 to 5 when given 5', () => {
-    expect(monkeyCountFrom(5)).toStrictEqual([1, 2, 3, 4, 5]);
-  });
-
-  test('returns an array of numbers from 1 to 20 when given 20', () => {
-    expect(monkeyCountFrom(20)).toStrictEqual([
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-    ]);
-  });
+  test.each`
+    input | expectedResult
+    ${5}  | ${[1, 2, 3, 4, 5]}
+    ${20} | ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
+  `(
+    'converts $input to $expectedResult with config: $configDescription',
+    ({ input, expectedResult }) => {
+      expect(monkeyCountFrom(input)).toStrictEqual(expectedResult);
+    },
+  );
 });
