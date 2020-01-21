da30e7c1773a234d6b36ff1d390a98d714c4233b
mpsujek <mateusz.psujek@gmail.com>
2020-01-21 23:26:00 +0100

    Change in naming display in test

--- js/count_monkeys.test.js
+++ js/count_monkeys.test.js
@@ -10,14 +10,16 @@ const functionsToTest = [
   monkeyCountDoWhile,
 ];
 
-functionsToTest.forEach((functionsToTest) => {
-  describe(`count_monkeys ${functionsToTest} function`, () => {
+const namesArray = ['for loop', 'from', 'do while'];
+
+functionsToTest.forEach((functionsToTest, i) => {
+  describe(`count_monkeys with ${namesArray[i]} function`, () => {
     test.each`
       input | expectedResult
       ${5}  | ${[1, 2, 3, 4, 5]}
       ${20} | ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
     `(
-      'converts $input to $expectedResult with config: $configDescription',
+      `converts $input to $expectedResult with ${namesArray[i]}`,
       ({ input, expectedResult }) => {
         expect(functionsToTest(input)).toStrictEqual(expectedResult);
       },
