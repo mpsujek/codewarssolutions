8456c9a9e31a15245aa5bea1a4e0119138d91ac9
mpsujek <mateusz.psujek@gmail.com>
2020-01-23 22:11:46 +0100

    Changes in functionNames use

--- js/count_monkeys.test.js
+++ js/count_monkeys.test.js
@@ -10,16 +10,14 @@ const functionsToTest = [
   monkeyCountDoWhile,
 ];
 
-const namesArray = ['for loop', 'from', 'do while'];
-
 functionsToTest.forEach((functionsToTest, i) => {
-  describe(`count_monkeys with ${namesArray[i]} function`, () => {
+  describe(`count_monkeys with ${functionsToTest.name} function`, () => {
     test.each`
       input | expectedResult
       ${5}  | ${[1, 2, 3, 4, 5]}
       ${20} | ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
     `(
-      `converts $input to $expectedResult with ${namesArray[i]}`,
+      `converts $input to $expectedResult with ${functionsToTest.name} ${i++}`,
       ({ input, expectedResult }) => {
         expect(functionsToTest(input)).toStrictEqual(expectedResult);
       },
