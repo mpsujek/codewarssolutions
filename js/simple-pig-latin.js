d0724265430196dc7843489324fec752b53be4ec
mpsujek <mateusz.psujek@gmail.com>
2020-02-10 23:56:20 +0100

    Changes in function naming and composition of function

--- js/simple-pig-latin.js
+++ js/simple-pig-latin.js
@@ -1,23 +1,25 @@
 // https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
 
-function pigIt(str){
-  const wordsArray = str.split(' ');
-  const resultArray = wordsArray.map((x, i) => {
-      const format = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-      let arr = x.split('');
-      const [first, ...rest] = arr;
-      arr = [...rest,first];
-      if(format.test(arr.join(''))){
-          return arr.join('');
-      }
-      else {
-      return `${arr.join('')}ay`;
-      }
-
-
-  })
+function pigIt(str) {
+  const splittedStrToArray = str.split(' ');
+  const resultArray = splittedStrToArray.map((x) => {
+    const regexToTestString = /[ !@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/;
+    const arrAfterSwitch = moveFirstLetterToEnd(x);
+    if (regexToTestString.test(arrAfterSwitch)) {
+      return arrAfterSwitch;
+    } else {
+      return `${arrAfterSwitch}ay`;
+    }
+  });
 
   return resultArray.join(' ');
 }
+
+const moveFirstLetterToEnd = (string) => {
+  let arr = string.split('');
+  const [first, ...rest] = arr;
+  arr = [...rest, first];
+  return arr.join('');
+};
 
 module.exports = pigIt;
--- js/simple-pig-latin.test.js
+++ js/simple-pig-latin.test.js
@@ -1,15 +1,11 @@
 const pigIt = require('./simple-pig-latin');
 
-
 describe('simple-pig-latin', () => {
-
   test('When it get Pig latin is cool it return  igPay atinlay siay oolcay', () => {
     expect(pigIt('Pig latin is cool')).toBe('igPay atinlay siay oolcay');
   });
 
-  test('When it get Pig latin is cool ?  it return  igPay atinlay siay oolcay ? test for special character in string', () => {
+  test('should not append -ay to punctuation', () => {
     expect(pigIt('Pig latin is cool ?')).toBe('igPay atinlay siay oolcay ?');
   });
-
-
 });
