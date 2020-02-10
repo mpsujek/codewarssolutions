fcf2cc924d46a9be6622ee3465653329c1f90376
mpsujek <mateusz.psujek@gmail.com>
2020-02-10 14:57:14 +0100

    Fix for regexp, merge with master, added tests, not jest are saying my function is not a function

---
+++ js/regex_password_validation.test.js
@@ -0,0 +1,12 @@
+const {
+  validatePassword
+} = require('./regex_password_validation');
+
+
+describe('Validate password by url', () => {
+
+  test('djI38D55 expect to be true', () => {
+    expect(validatePassword('djI38D55')).toBe(true);
+  });
+
+});
