27175cb6f283639b886a92bd74df5fdc12029ce4
mpsujek <mateusz.psujek@gmail.com>
2020-02-11 14:56:56 +0100

    Cosmetic changes to improve test and semantic of code

--- js/count-letter-in-string.js
+++ js/count-letter-in-string.js
@@ -1,13 +1,12 @@
 // https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
 
 function count(string) {
-  const lettersArr = string.split('');
-  let lettersCount = {};
-  for (let i = 0; i < lettersArr.length; i++) {
-    if (lettersCount[lettersArr[i]] == undefined) {
-      lettersCount[lettersArr[i]] = 1;
+  const lettersCount = {};
+  for (let i = 0; i < string.length; i++) {
+    if (lettersCount[string[i]] == undefined) {
+      lettersCount[string[i]] = 1;
     } else {
-      lettersCount[lettersArr[i]]++;
+      lettersCount[string[i]]++;
     }
   }
   return lettersCount;
