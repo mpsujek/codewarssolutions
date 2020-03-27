6d671f767c98c597018c5b11b740c2db3a831e40
mpsujek <mateusz.psujek@gmail.com>
2020-03-27 21:25:22 +0100

    Fix for failing test:

--- js/regex_password_validation.test.js
+++ js/regex_password_validation.test.js
@@ -1,12 +1,7 @@
-const {
-  validatePassword
-} = require('./regex_password_validation');
-
+const validatePassword = require('./regex_password_validation');
 
 describe('Validate password by url', () => {
-
   test('djI38D55 expect to be true', () => {
     expect(validatePassword('djI38D55')).toBe(true);
   });
-
 });
