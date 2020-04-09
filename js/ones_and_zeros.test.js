636fce4776c68d64dac83946e7cf387dc3be1a9b
mpsujek <mateusz.psujek@gmail.com>
2020-04-09 22:30:51 +0200

    Changed naming of tests cases and fixed 555 vulnerabilities

--- js/ones_and_zeros.test.js
+++ js/ones_and_zeros.test.js
@@ -1,10 +1,10 @@
-const binaryArrayToNumber = require("./ones_and_zeros");
+const binaryArrayToNumber = require('./ones_and_zeros');
 
-describe("binaryArrayToNumber", () => {
-  test("When it gets array zeros and ones of should return decimal number", () => {
+describe('binaryArrayToNumber', () => {
+  test('When it gets array [0, 0, 0, 1] should return decimal number 1', () => {
     expect(binaryArrayToNumber([0, 0, 0, 1])).toBe(1);
   });
-  test("When it gets array zeros and ones of should return decimal number", () => {
+  test('When it gets array [1, 1, 1, 1] should return decimal number 15', () => {
     expect(binaryArrayToNumber([1, 1, 1, 1])).toBe(15);
   });
 });
